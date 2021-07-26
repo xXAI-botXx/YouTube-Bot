a9b235aab8f2450f35c8d4031fbabd72335326db
Tobia Ippolito <tobiaippolito@outlook.de>
2021-07-26 18:29:09 +0200

    - rgb upgrade - button highlighting changes

--- gui.py
+++ gui.py
@@ -1,18 +1,28 @@
 import tkinter as tk
 import main
 from datetime import datetime as dt
+import random
 
-def rgb2hex(rgb):
+def rgb2hex_alt(rgb):
     """translates an rgb tuple of int to a tkinter friendly color code
     """
     r, g, b = rgb
     return f'#{r:02x}{g:02x}{b:02x}'
 
-def rgb2hex_alt(r, g, b):
+def rgb2hex(r, g, b):
     """translates an rgb tuple of int to a tkinter friendly color code
     """
     hex_n = format(r, 'x') + format(g, 'x') + format(b, 'x')
     return f'#{hex_n}'
+
+def hex2rgb(x):
+    x = x.replace("#", "")
+    x = x.replace("0x", "")
+    r = x[0:2]
+    g = x[2:4]
+    b = x[4:6]
+    dec_n = (int(format(int(r, 16), 'd')), int(format(int(g, 16), 'd')), int(format(int(b, 16), 'd')))
+    return dec_n
 
 
 class Widget_With_Space(tk.Frame):
@@ -20,6 +30,9 @@ class Widget_With_Space(tk.Frame):
         super().__init__(parent)
         self.btn_color = '#A9BCF5'
         self.btn_color_2 = '#819FF7'
+        self.is_hovered = False
+        self.xspace = xspace
+        self.yspace = yspace
         
         if command == None:    # no btn
             self.widget = widget(self, bg=bg, state="disabled")    #justify=tk.LEFT
@@ -30,10 +43,13 @@ class Widget_With_Space(tk.Frame):
         self.widget.pack(expand=True, fill=tk.BOTH, padx=xspace, pady=yspace)
 
     def on_enter(self, event):
+        self.is_hovered = True
         btn = event.widget
-        btn.config(bg=self.btn_color_2)
+        #btn.config(bg=self.btn_color_2)
+        btn.config(bg="#FA5858")
 
     def on_leave(self, event):
+        self.is_hovered = False
         btn = event.widget
         btn.config(bg=self.btn_color)
 
@@ -60,7 +76,12 @@ class Widget_With_Space(tk.Frame):
         self.widget.delete("0.0", self.widget.index("end"))
         self.widget.insert("0.0", txt)
         self.widget.config(state="disabled")
-        
+
+    def set_color(self, c1:str, c2:str):
+        self.btn_color = c1
+        self.btn_color_2 = c2    # now geupdated
+        if not self.is_hovered:
+            self.widget.config(bg=c1)
 
 
 class Bot_Viewer(tk.Frame):
@@ -71,13 +92,22 @@ class Bot_Viewer(tk.Frame):
         self.rows = 6
         self.columns = 2 #2
         self.control = main.Bot_System_GUI(self)
-        self.btn_color = '#A9BCF5'
-        self.btn_color_2 = '#819FF7'
+        r = random.randint(30, 255)
+        g = random.randint(30, 255)
+        b = random.randint(30, 255)
+        self.color_factor = 10
+        self.r_reduce = random.randint(-5, 5)
+        self.g_reduce = random.randint(-5, 5)
+        self.b_reduce = random.randint(-5, 5)
+        self.btn_color = rgb2hex(r, g, b)  # random init
+        self.btn_color_2 = rgb2hex(r-self.color_factor, g-self.color_factor, b-self.color_factor)
         self.output = ""
         self.pause = False
         self.history_is_shown = False
+        self.buttons = []
         self.build_widgets()
         self.build_grow_factor()
+        self.after(1000, self.color_update)
 
     def build_widgets(self):
         # -> farbe soll sich verändern, aber evtl immer nur blasse farben
@@ -85,6 +115,7 @@ class Bot_Viewer(tk.Frame):
         #self.label_output.grid(row=0, column=1, rowspan=4, sticky='NESW') #, padx=(50, 0))
         self.txt_output = Widget_With_Space(tk.Text, "", "#BDBDBD", xspace=(10, 10), yspace=(10, 10), parent=self)
         self.txt_output.grid(row=0, column=1, rowspan=6, sticky='NESW') #, padx=(50, 0))
+        self.txt_output.set_color(self.btn_color, self.btn_color_2)
         # Am Anfang Bot Explaination hinzufügen
         self.output += f"Bot-System is now live ({dt.now().hour:02}:{dt.now().minute:02} Clock)"
         self.write_in_output()
@@ -92,21 +123,33 @@ class Bot_Viewer(tk.Frame):
         #self.btn_run = tk.Button(self, text='run', bg='#FA5858', command=self.event_run)
         self.btn_run = Widget_With_Space(tk.Button, text='run', bg=self.btn_color, command=self.event_run, xspace=(10, 10), yspace=(10, 10), parent=self)
         self.btn_run.grid(row=0, column=0, sticky='NESW') 
+        self.btn_run.set_color(self.btn_color, self.btn_color_2)
+        self.buttons += [self.btn_run]
 
         self.btn_stop = Widget_With_Space(tk.Button, text='stop', bg=self.btn_color, command=self.event_stop, xspace=(10, 10), yspace=(0, 10), parent=self)
         self.btn_stop.grid(row=1, column=0, sticky='NESW')
+        self.btn_stop.set_color(self.btn_color, self.btn_color_2)
+        self.buttons += [self.btn_stop]
 
         self.btn_debug = Widget_With_Space(tk.Button, text='debug', bg=self.btn_color, command=self.event_debug, xspace=(10, 10), yspace=(0, 10), parent=self)
         self.btn_debug.grid(row=2, column=0, sticky='NESW')
+        self.btn_debug.set_color(self.btn_color, self.btn_color_2)
+        self.buttons += [self.btn_debug]
 
         self.btn_pause = Widget_With_Space(tk.Button, text='pause', bg=self.btn_color, command=self.event_pause, xspace=(10, 10), yspace=(0, 10), parent=self)
         self.btn_pause.grid(row=3, column=0, sticky='NESW')
+        self.btn_pause.set_color(self.btn_color, self.btn_color_2)
+        self.buttons += [self.btn_pause]
 
         self.btn_history = Widget_With_Space(tk.Button, text='history', bg=self.btn_color, command=self.event_history, xspace=(10, 10), yspace=(0, 10), parent=self)
         self.btn_history.grid(row=4, column=0, sticky='NESW')
+        self.btn_history.set_color(self.btn_color, self.btn_color_2)
+        self.buttons += [self.btn_history]
 
         self.btn_exit = Widget_With_Space(tk.Button, text='exit', bg=self.btn_color, command=self.event_exit, xspace=(10, 10), yspace=(0, 10), parent=self)
         self.btn_exit.grid(row=5, column=0, sticky='NESW')
+        self.btn_exit.set_color(self.btn_color, self.btn_color_2)
+        self.buttons += [self.btn_exit]
 
     def build_grow_factor(self):
         for r in range(self.rows):
@@ -160,6 +203,50 @@ class Bot_Viewer(tk.Frame):
 
     def clear_output(self):
         self.output = ""
+
+    def color_update(self):
+        min_lim = 30
+        max_lim = 255
+        # color update
+        r, g, b = hex2rgb(self.btn_color)
+
+        r -= self.r_reduce
+        if r < min_lim:
+            self.r_reduce = - self.r_reduce
+            r -= self.r_reduce*2
+        elif r > max_lim:
+            self.r_reduce = - self.r_reduce
+            r -= self.r_reduce*2
+
+        g -= self.g_reduce
+        if g < min_lim:
+            self.g_reduce = - self.g_reduce
+            g -= self.g_reduce
+        elif g > max_lim:
+            self.g_reduce = - self.g_reduce
+            g -= self.g_reduce
+
+        b -= self.b_reduce
+        if b < min_lim:
+            self.b_reduce = - self.b_reduce
+            b -= self.b_reduce
+        elif b > max_lim:
+            self.b_reduce = - self.b_reduce
+            b -= self.b_reduce
+
+        self.btn_color = rgb2hex(r, g, b)
+        c1 = rgb2hex(r, g, b)
+        # hover color update
+        self.btn_color_2 = rgb2hex(r-self.color_factor, g-self.color_factor, b-self.color_factor)
+        c2 = rgb2hex(r-self.color_factor, g-self.color_factor, b-self.color_factor)
+
+        # btn update
+        for btn in self.buttons:
+            btn.set_color(c1, c2)
+        self.txt_output.set_color(c1, c2)
+        
+        # repeat
+        self.after(200, self.color_update)
 
 def start():
     root = tk.Tk()
